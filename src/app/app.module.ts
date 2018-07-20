708b33d855ab2e3ad271f72290a305e97c560135
satj86 <ssjootle@gmail.com>
2018-07-20 23:01:31 +0100

    add material design components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { MdcAppBarModule, MdcTypographyModule, MdcIconModule } from '@angular-mdc/web';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdcAppBarModule,
+    MdcTypographyModule,
+    MdcIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
